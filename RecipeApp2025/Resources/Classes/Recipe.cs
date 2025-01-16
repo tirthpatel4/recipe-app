f2d8d551c574a7af9245ccfef42e7b9611cc8016
SamIAmNot52 <sam.baccei@gmail.com>
2025-01-15 19:25:43 -0500

    Create Recipe.cs

---
+++ RecipeApp2025/Resources/Classes/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp2025.Resources.Classes
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+          
+        public Recipe(string name)
+        {
+            Name = name;
+        }
+    }
+}
