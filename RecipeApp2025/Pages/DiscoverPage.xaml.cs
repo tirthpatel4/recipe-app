80760dbad8503eba00881d2e390dfc17edc81b13
SamIAmNot52 <sam.baccei@gmail.com>
2025-01-16 16:40:10 -0500

    Recipe Feed in Discover page.

--- RecipeApp2025/Pages/DiscoverPage.xaml.cs
+++ RecipeApp2025/Pages/DiscoverPage.xaml.cs
@@ -1,9 +1,71 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using RecipeApp2025.Resources.Classes;
 namespace RecipeApp2025.Pages;
-
-public partial class DiscoverPage : ContentPage
+public partial class DiscoverPage : ContentPage, INotifyPropertyChanged
 {
+	private string[] Names = {"Spaghetti Carbonara",
+							"Chicken Alfredo",
+							"Beef Stroganoff",
+							"Vegetarian Chili",
+							"Grilled Salmon",
+							"Garlic Butter Shrimp",
+							"Stuffed Bell Peppers",
+							"Teriyaki Chicken",
+							"Eggplant Parmesan",
+							"Lemon Herb Roasted Chicken",};
+	public List<Recipe> Recipes;
+
+	/* variables for handing the save button */
+	private bool _isSaved;
+	private string _saveIcon;
+	public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+
+	public string SaveIcon
+	{
+		get => _saveIcon;
+		set
+		{
+			_saveIcon = value;
+			OnPropertyChanged(nameof(SaveIcon));
+		}
+	}
 	public DiscoverPage()
 	{
+		Recipes = new List<Recipe>();
 		InitializeComponent();
-	}
+
+        /* temporary hard coded data */
+        for (int i = 1; i < 10; i++)
+		{
+			Recipes.Add(new Recipe(Names[i]));
+
+        }
+
+		DiscoverFeed.ItemsSource = Recipes;
+		//DiscoverRecipeItem	\
+
+
+		_isSaved = false;
+		SaveIcon = "saveempty.png";
+
+		
+    }
+	
+	
+	private void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		_isSaved = !_isSaved;
+        SaveIcon = _isSaved ? "savefull.png" : "saveempty.png";
+    }
+
+    protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
 }
+
+
--- RecipeApp2025/Resources/Classes/Recipe.cs
+++ RecipeApp2025/Resources/Classes/Recipe.cs
@@ -9,10 +9,17 @@ namespace RecipeApp2025.Resources.Classes
     public class Recipe
     {
         public string Name { get; set; }
-          
+        public string Img_url { get; set; }
+        public int Total_time { get; set; }
+        public int Num_people_served { get; set; }
+        public bool isSaved { get; set; }
         public Recipe(string name)
         {
             Name = name;
+            Img_url = "https://img.spoonacular.com/recipes/716429-90x90.jpg";
+            Num_people_served = 2;
+            Total_time = 45;
+            isSaved = false;
         }
     }
 }
