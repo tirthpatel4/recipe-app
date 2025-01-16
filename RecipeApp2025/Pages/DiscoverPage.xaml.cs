a95988ab5b34b0161b924bf3f4d4a07efde3ab6d
Quinton Holmes <quintonrholmes30@gmail.com>
2025-01-15 20:10:45 -0500

    Added spoonacular api to discover page.

--- RecipeApp2025/Pages/DiscoverPage.xaml.cs
+++ RecipeApp2025/Pages/DiscoverPage.xaml.cs
@@ -1,9 +1,30 @@
+using System.Diagnostics;
+
 namespace RecipeApp2025.Pages;
 
 public partial class DiscoverPage : ContentPage
 {
-	public DiscoverPage()
+    private readonly HttpClient _httpClient = new HttpClient();
+	private const string ApiKey = "REQUEST API KEY FROM QUINTON! DM VIA DISCORD.";
+	private const string BaseUrl = "https://api.spoonacular.com/";
+    public DiscoverPage()
 	{
 		InitializeComponent();
 	}
+
+    private async Task<string> GetRecipesAsync()
+    {
+        var url = $"{BaseUrl}recipes/complexSearch?apiKey={ApiKey}";
+        var response = await _httpClient.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+        else {
+            Debug.WriteLine("Error: " + response.StatusCode);
+            return null;
+        }
+    }
+
 }
