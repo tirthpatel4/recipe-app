baed097707267c86da39d30aa2590871ae7e055d
Quinton Holmes <quintonrholmes30@gmail.com>
2025-01-17 18:12:20 -0500

    Getting recipes now returns titles only
    Change later.

--- RecipeApp2025/Pages/DiscoverPage.xaml.cs
+++ RecipeApp2025/Pages/DiscoverPage.xaml.cs
@@ -1,25 +1,37 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices.JavaScript;
+using Newtonsoft.Json.Linq;
 
 namespace RecipeApp2025.Pages;
 
 public partial class DiscoverPage : ContentPage
 {
     private readonly HttpClient _httpClient = new HttpClient();
-	private const string ApiKey = "REQUEST API KEY FROM QUINTON! DM VIA DISCORD.";
+	private const string ApiKey = "e9086f66a8184e88a43bac38c112dba0";
 	private const string BaseUrl = "https://api.spoonacular.com/";
     public DiscoverPage()
 	{
 		InitializeComponent();
 	}
 
-    private async Task<string> GetRecipesAsync()
+    private async Task<List<string>> GetRecipesAsync()
     {
         var url = $"{BaseUrl}recipes/complexSearch?apiKey={ApiKey}";
         var response = await _httpClient.GetAsync(url);
         if (response.IsSuccessStatusCode)
         {
             var content = await response.Content.ReadAsStringAsync();
-            return content;
+            var json = JObject.Parse(content);
+            var titles = json["results"].Select(r => r["title"].ToString()).ToList();
+
+            // DEBUG ONLY!
+            //foreach (string t in titles)
+            //{
+            //    Debug.Write(t);
+            //}
+            //Debug.WriteLine("");
+
+            return titles;
         }
         else {
             Debug.WriteLine("Error: " + response.StatusCode);
