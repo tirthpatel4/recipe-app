aba053b29a42aa6f50d393c5f2a8da137a31f9a4
SamIAmNot52 <sam.baccei@gmail.com>
2025-01-23 17:50:45 -0500

    Added detail page to show expanded recipe information. Added Ingredient class for later development.

--- RecipeApp2025/Pages/DetailPage.xaml.cs
+++ RecipeApp2025/Pages/DetailPage.xaml.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using RecipeApp2025.Resources.Classes;
+
 namespace RecipeApp2025.Pages;
 
 public partial class DetailPage : ContentPage
 {
+   
     public DetailPage()
     {
+
         InitializeComponent();
+        //Set Binding context to global variable
+        BindingContext = App.CurrentRecipe;
     }
+
+    
+    
 }
--- RecipeApp2025/Resources/Classes/Recipe.cs
+++ RecipeApp2025/Resources/Classes/Recipe.cs
@@ -1,25 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RecipeApp2025.Resources.Classes
+
+namespace RecipeApp2025.Resources.Classes 
 {
     public class Recipe
     {
         public string Name { get; set; }
+        public string Large_img_url { get; set; }
         public string Img_url { get; set; }
         public int Total_time { get; set; }
+        public int Cook_time { get; set; }
+        public int Prep_time { get; set; }
         public int Num_people_served { get; set; }
         public bool isSaved { get; set; }
+
         public Recipe(string name)
         {
             Name = name;
             Img_url = "https://img.spoonacular.com/recipes/716429-90x90.jpg";
+            Large_img_url = "https://img.spoonacular.com/recipes/1697885-556x370.jpg";
             Num_people_served = 2;
             Total_time = 45;
+            Cook_time = 25;
+            Prep_time = 20;
             isSaved = false;
+        }
+    }
+
+    public class Ingredient
+    {
+        public string I_Name { get; set; }
+        public int I_Count { get; set; }
+        public int I_Amount { get; set; }
+        public string I_Unit { get; set; }
+
+        public Ingredient(string name, int count, int amount, string unit)
+        {
+            I_Name = name;
+            I_Count = count;
+            I_Amount = amount;
+            I_Unit = unit;
         }
     }
 }
