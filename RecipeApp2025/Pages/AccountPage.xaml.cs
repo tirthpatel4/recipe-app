36a262efc624a20bdd164f06bf9d018fe0792282
Quinton Holmes <quintonrholmes30@gmail.com>
2025-01-30 18:45:02 -0500

    Account events and text entries

--- RecipeApp2025/Pages/AccountPage.xaml.cs
+++ RecipeApp2025/Pages/AccountPage.xaml.cs
@@ -1,9 +1,32 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
 namespace RecipeApp2025.Pages;
 
 public partial class AccountPage : ContentPage
 {
-	public AccountPage()
+	private string _username;
+	private string _password;
+    public AccountPage()
 	{
 		InitializeComponent();
+        _username = String.Empty;
+        _password = String.Empty;
 	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        UsernameEntry.TextChanged += OnUsernameChanged;
+        PasswordEntry.TextChanged += OnPasswordChanged;
+    }
+    private void OnUsernameChanged(object sender, TextChangedEventArgs e)
+    {
+        _username = e.NewTextValue;
+    }
+
+    private void OnPasswordChanged(object sender, TextChangedEventArgs e)
+    {
+        _password = e.NewTextValue;
+    }
 }
