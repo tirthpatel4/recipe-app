6e5c82542cca00af646552dc2cc3cd4c1b1cb9bc
SamIAmNot52 <sam.baccei@gmail.com>
2025-02-10 19:17:40 -0500

    Changed some formatting on main page and detail page. Added reformatting to main page so that elements respond to changes in orientation.

--- RecipeApp2025/Pages/MainPage.xaml.cs
+++ RecipeApp2025/Pages/MainPage.xaml.cs
@@ -1,12 +1,33 @@
-﻿namespace RecipeApp2025.Pages
+﻿using System.Runtime.CompilerServices;
+
+namespace RecipeApp2025.Pages
 {
     public partial class MainPage : ContentPage
     {
 
+
         public MainPage()
         {
             InitializeComponent();
-            
+            BindingContext = this;
+            this.SizeChanged += OnSizeChanged;
+        }
+        private void OnSizeChanged(object sender, EventArgs e)
+        {
+            SetStackLayoutOrientation();
+        }
+
+        private void SetStackLayoutOrientation()
+        {
+            if (IsInPortrait())
+            {
+                ButtonStackLayout.Orientation = StackOrientation.Vertical;
+            }
+            else
+            {
+                ButtonStackLayout.Orientation = StackOrientation.Horizontal;
+            }
+
         }
         private async void OnDiscoverButtonClicked(object sender, EventArgs e)
         {
@@ -17,6 +38,14 @@
         {
             await Shell.Current.GoToAsync("/SavedRecipesPage");
         }
+
+        private Boolean IsInPortrait()
+        {
+            double width = Microsoft.Maui.Devices.DeviceDisplay.MainDisplayInfo.Width;
+            double height = Microsoft.Maui.Devices.DeviceDisplay.MainDisplayInfo.Height;
+            return (width <= height);
+        }
+
     }
 
 }
